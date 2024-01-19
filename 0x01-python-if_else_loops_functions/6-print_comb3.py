66be1e1fa657da2c9c9c223a82ec71d5866a7807
brio kchr <b.kchr4@gmail.com>
2024-01-19 07:45:26 -0800

    Prints hexadecimal and number combinations with various separators

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in range(8):
     for x in range(i, 10):
         if x == i:
             continue
-        print("{:d}{:d}".format(i, x), end = ", ")
+        print("{:d}{:d}".format(i, x), end=", ")
 print("{:d}{:d}".format(i + 1, x))
