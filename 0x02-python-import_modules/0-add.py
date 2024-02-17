d4cca8cf81efe291d739181f7c306bb96fc542f5
brio kchr <b.kchr4@gmail.com>
2024-02-17 01:57:32 -0800

    Imports function from a file and uses it in my code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
+"""Used to execute some files if the code was run
+directly and not imported"""
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
