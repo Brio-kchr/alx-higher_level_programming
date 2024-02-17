1785289c1458a16f1bd950ef124718c2da6722cd
brio kchr <b.kchr4@gmail.com>
2024-02-17 03:26:17 -0800

    Prints the number of and list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Used to execute some files if the code was run
+directly and not imported"""
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 0:
+        print("0 arguments.")
+    elif len(sys.argv) == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(len(sys.argv)))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {}".format(i, sys.argv[i]))
