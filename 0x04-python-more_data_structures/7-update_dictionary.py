669645d5331c08de4173b5faf00c1ba064e1517e
brio kchr <b.kchr4@gmail.com>
2024-02-19 13:41:21 -0800

    function that replaces or adds key/value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+"""function that adds or replaces a keny value pair"""
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
