561c51012ab7f0a3280c927787af7e8abd4ae6e0
brio kchr <b.kchr4@gmail.com>
2024-02-19 07:34:14 -0800

    Search and replace a value from a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 
-"""function that replaces all occurrences of an element by another in a new list"""
+"""function that replaces all occurrences of an
+element by another in a new list"""
+
 
 def search_replace(my_list, search, replace):
     res = [x if x != search else replace for x in my_list]
