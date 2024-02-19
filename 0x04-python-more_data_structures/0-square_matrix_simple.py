5565931f718a6866825a2dd9ea2762e7c7c61fa9
brio kchr <b.kchr4@gmail.com>
2024-02-19 06:29:07 -0800

    Squaring a multidimensional list

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
+
 """function that computes the square value of all integers of a matrix"""
+
+
 def square_matrix_simple(matrix=[]):
     res = []
     for i in matrix:
-        res.append(list(map(lambda x : x * x, i)))
+        res.append(list(map(lambda x: x * x, i)))
     return res
