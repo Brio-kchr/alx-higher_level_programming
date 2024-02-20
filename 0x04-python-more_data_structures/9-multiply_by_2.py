242a39f78c136e878f14ea34ebff206eb31aed9f
brio kchr <b.kchr4@gmail.com>
2024-02-20 06:40:32 -0800

    Checking checker errors

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-
-
-"""function that returns a new dictionary with all values multiplied by 2"""
-
-
 def multiply_by_2(a_dictionary):
-    'Create a new dict store doubled values'
     new_dict = dict()
-    'Loop through your dict, double each value
-    and update our new dict'
     for key, value in a_dictionary.items():
         new_dict.update({key: value*2})
     return new_dict
