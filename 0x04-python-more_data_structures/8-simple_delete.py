6f076f5d58292588d494ceda9d3aa549cd96c817
brio kchr <b.kchr4@gmail.com>
2024-02-20 05:46:22 -0800

    Function deletes a key value pair from a dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-
-
-"""function that deletes a key value pair"""
-
-
 def simple_delete(a_dictionary, key=""):
+    """Function for deleting a value from a dict
+
+    Args:
+        a_dictionary: the dictionary to be updated
+        key: The key of the element to be deleted
+
+    Returns:
+        The dictionary without the deleted key
+    """
     if key in a_dictionary:
         del a_dictionary[key]
     return a_dictionary
