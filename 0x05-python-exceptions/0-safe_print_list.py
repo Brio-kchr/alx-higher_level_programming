6f8359f8b6c2f2c254c705b6813d30cbeabdbd91
brio kchr <b.kchr4@gmail.com>
2024-02-21 00:18:12 -0800

    Handling python exeptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        res = 0 
+        res = 0
         for i in range(0, x):
             print(my_list[i], end="")
             res += 1
